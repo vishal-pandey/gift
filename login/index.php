43b8f49f9adb6fd384d3380c010ebd36e4eaad4a
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 21:58:36 +0530

    update by script

--- login/index.php
+++ login/index.php
@@ -1,12 +1,23 @@
 <?php
+	include '../config.php';
 	$json = file_get_contents("php://input");
 	if ($json) {
 		$obj = json_decode($json);
-		if ($obj->mobile == "9717130893" && $obj->pwd == "Vishal@123") {
-			echo "success";
-		}else{
-			echo "fail";
+		$mobile = $obj->mobile;
+		$password = $obj->password;
+		$sql = "select * from user where mobile = '{$mobile}'";
+		$result = $conn->query($sql);
+		if ($result->num_rows >0) {
+			$row = $result->fetch_assoc();
+			if ($row['password'] == $password) {
+				echo "succes";
+			}
 		}
+		// if ($obj->mobile == "9717130893" && $obj->pwd == "Vishal@123") {
+		// 	echo "success";
+		// }else{
+		// 	echo "fail";
+		// }
 	}
 	
 ?>
