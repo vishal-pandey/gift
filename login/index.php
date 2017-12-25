a8aa570da5732b99b68400fd8ffa0a73bd7ddb6d
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 16:00:23 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -1,17 +1,12 @@
-
 <?php
-
-	// header('Access-Control-Allow-Origin: *');
-	// header('Access-Control-Allow-Methods: GET, POST, OPTION'); 
-	// header('Access-Control-Allow-Headers: *');
-
-	// header('Content-Type: application/json');
-	// $data = json_decode(file_get_contents("php://input"));
-	$data = file_get_contents("php://input");
-	$obh = json_decode($data, TRUE);
-	echo $obh;
-	// echo $data['mobile'];
-	// echo "string";
-	// echo $data['pwd'];
-	// echo key($_POST);
+	$json = file_get_contents("php://input");
+	if ($data) {
+		$obj = json_decode($json);
+		if ($obj->mobile == "9717130893" && $obj->pwd == "Vishal@123") {
+			echo "success";
+		}else{
+			echo "fail";
+		}
+	}
+	
 ?>
