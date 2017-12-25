d82cef8285df5a2e060209cdd63b271a4630f755
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 14:35:23 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -1,6 +1,6 @@
 <?php 
 	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: GET, POST'); 
+	header('Access-Control-Allow-Methods: GET, POST, OPTION'); 
 	header('Access-Control-Allow-Headers: X-PINGARUNER');
 ?>
 <?php
