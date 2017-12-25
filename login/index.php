1a0adcffae6530754f1debee53eb7b8056dc3409
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 15:46:25 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
 	// header('Content-Type: application/json');
 	// $data = json_decode(file_get_contents("php://input"));
 	$data = file_get_contents("php://input");
-	echo $data;
+	echo $data->mobile;
 	// echo $data['mobile'];
 	echo "string";
 	// echo $data['pwd'];
