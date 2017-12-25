bbb80fdb88925c1c309edeb8dc2b0001701db50c
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-26 01:20:57 +0530

    update by script

---
+++ jwt.php
@@ -0,0 +1,29 @@
+<?php
+use \Firebase\JWT\JWT;
+
+$key = "example_key";
+$token = array(
+    "iss" => "http://example.org",
+    "aud" => "http://example.com",
+    "iat" => 1356999524,
+    "nbf" => 1357000000
+);
+
+
+$jwt = JWT::encode($token, $key);
+$decoded = JWT::decode($jwt, $key, array('HS256'));
+
+print_r($decoded);
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+$decoded_array = (array) $decoded;
+
+
+JWT::$leeway = 60; // $leeway in seconds
+$decoded = JWT::decode($jwt, $key, array('HS256'));
+
+?>
