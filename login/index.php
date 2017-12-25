54dde276f627814d169a0f8f60c361e0175224fd
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 16:01:02 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -1,6 +1,6 @@
 <?php
 	$json = file_get_contents("php://input");
-	if ($data) {
+	if ($json) {
 		$obj = json_decode($json);
 		if ($obj->mobile == "9717130893" && $obj->pwd == "Vishal@123") {
 			echo "success";
