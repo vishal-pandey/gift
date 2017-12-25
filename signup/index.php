fff5fe58f1060f858cb4a8c73bc5c6ef7976e921
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 21:33:20 +0530

    update by script

--- signup/index.php
+++ signup/index.php
@@ -1,4 +1,16 @@
 <?php
  include "../config.php";
- 
+ $json = file_get_contents("php://input");
+ if ($json) {
+		$obj = json_decode($json);
+		$id = uniqid("u_");
+		$name = $obj->name;
+		$mobile = $obj->mobile;
+		$email = $obj->email;
+		$password = $obj->password;
+		$sql = "insert into user (id, name, mobile, email, password) VALUES ('{$id}','{$name}','{$mobile}','{$email}','{$password}')";
+		if($conn->query($sql)){
+			echo "success";
+		}
+	}
 ?>
