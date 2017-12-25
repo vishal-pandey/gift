e07223f77623448af51740f57c2733d0872958fc
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 13:04:43 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -1,7 +1,11 @@
-<?php header('Access-Control-Allow-Origin: *'); ?>
+<?php 
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: GET, POST'); 
+?>
 <?php
 	session_start();
 	echo $_POST['mobile'];
+	echo $_POST['pwd'];
 ?>
 {
 	"mobile": 9717130893,
