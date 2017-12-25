7523d3fb6fd1f338b29ac53c81123fd72ea6069b
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 21:27:37 +0530

    update by script

--- setup.php
+++ setup.php
@@ -3,6 +3,6 @@
 	$sql = "drop table user";
 	$conn->query($sql);
 
-	$sql = "create table user(id varchar(15) primary key, name varchar(250), mobile bigint(10), email varchar(500), address text)"; 
+	$sql = "create table user(id varchar(15) primary key, name varchar(250), mobile bigint(10), email varchar(500), address text, password varchar(33))"; 
 	$conn->query($sql);
 ?>
