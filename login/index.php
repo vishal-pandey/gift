0c4c79eca1b0bb7f73d29bbfeff808c15e2028c4
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-25 15:35:33 +0530

    first commit

--- login/index.php
+++ login/index.php
@@ -1,6 +1,6 @@
 <?php
 	// header('Content-Type: application/json');
-	$_POST = json_decode(file_get_contents("php://input"));
+	$data = json_decode(file_get_contents("php://input"));
 	echo $_POST['mobile'];
 	echo "string";
 	echo $_POST['pwd'];
