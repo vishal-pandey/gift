ebb30680ab7c2d8d0daa062ee47616696c2f6fde
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-27 01:16:10 +0530

    update by script

--- user/index.php
+++ user/index.php
@@ -1,11 +1,15 @@
 <?php
-	$some_name = session_name("some_name");
-	session_set_cookie_params(0, '/', 'localhost:4200');
-	session_start();
-	// $_SESSION['user']="vishal";
-	if (isset($_SESSION['user'])) {
-		echo $_SESSION['user'];
-	}else{
-		echo "notlogged";
+	use \Firebase\JWT\JWT;
+	require '../vendor/autoload.php';
+	include '../config.php';
+
+
+	$json = file_get_contents("php://input");
+	if ($json) {
+		$obj = json_decode($json);
+		$key = "Vishalisgre8";
+		$jwt = $obj->token;
+		$decoded = JWT::decode($jwt, $key, array('HS256'));
+		echo json_encode($decoded);
 	}
 ?>
