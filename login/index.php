67ba02808c93625c20f0c0cebea27204e4599cb3
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-27 01:25:37 +0530

    update by script

--- login/index.php
+++ login/index.php
@@ -16,7 +16,7 @@
 				// echo "success";
 				$key = "Vishalisgre8";
 				$token = array(
-						"id" => $row['id'];
+						"id" => $row['id'],
 				    "name" => $row['name'],
 				    "email" => $row['email'],
 				    "address" =>$row['address']
