2cd0fa97aecee38344d6d5d8b391b801e94cb7c9
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-27 02:54:00 +0530

    update by script

--- setup.php
+++ setup.php
@@ -1,8 +1,11 @@
 <?php
 	include './config.php';
-	$sql = "drop table user";
-	$conn->query($sql);
+	// $sql = "drop table user";
+	// $conn->query($sql);
 
 	$sql = "create table user(id varchar(15) primary key, name varchar(250), mobile bigint(10), email varchar(500), address text, password varchar(33))"; 
+	$conn->query($sql);
+
+	$sql = "create table cart(id varchar(15), value text)";
 	$conn->query($sql);
 ?>
