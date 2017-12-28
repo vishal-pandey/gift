4de4c2c8e902ac27d0c867bae3069608ecb84da6
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-28 17:32:47 +0530

    update by script

--- order/place.php
+++ order/place.php
@@ -8,16 +8,20 @@
 	$json = file_get_contents("php://input");
 	if ($json) {
 		$obj = json_decode($json);
-		print_r($obj);
 		$key = "Vishalisgre8";
 		$jwt = $obj->token;
 		$decoded = JWT::decode($jwt, $key, array('HS256'));
 
 		$id = uniqid("o_");
+		echo $id;
 		$uid = $decoded->id;
+		echo $uid;
 		$odate = date("Y-m-d H:i:s");
+		echo $odate;
 		$theorder = $obj->order;
+		echo $theorder;
 		$cost = $obj->cost;
+		echo $cost;
 		$sql = "insert into order values ('{$id}','{$uid}','{$odate}','{$theorder}','{$cost}')";
 		echo $sql;
 		if($conn->query($sql)){
