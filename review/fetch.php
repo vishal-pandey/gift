056af411f1d45dae80bbdd9bb3b1144582f543ab
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-29 21:13:45 +0530

    update by script

---
+++ review/fetch.php
@@ -0,0 +1,37 @@
+<?php
+	include '../config.php';
+
+	date_default_timezone_set("Asia/Kolkata");
+
+	$json = file_get_contents("php://input");
+	if ($json) {
+		$obj = json_decode($json);
+		
+		$pid = $obj->pid;
+		$status = 'w';
+
+		$sql = "select * from review where pid = '{$pid}' and status = 'w'";
+		$result = $conn->query($sql);
+
+		echo "[";
+		while($row = $result->fetch_assoc()){
+			$rdate = $row['rdate'];
+			$name = $row['name'];
+			$comment = $row['comment'];
+			$rating = $row['rating'];
+			echo '{
+							"rdate": "{$rdate}",
+							"name": "{$name}",
+							"comment": "{$comment}",
+							"rating": "{$rating}",
+						},';
+		}
+		echo "{}]";
+
+		// $sql = "insert into review (id, pid, rdate, name, email, comment, rating, status) values ('{$id}','{$pid}','{$rdate}','{$name}','{$email}','{$comment}','{$rating}','{$status}')";
+
+		// if($conn->query($sql)){
+		// 	echo "success";
+		// }
+	}
+?>
