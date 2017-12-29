dff2b86db2b42eeb3b607d58d67b29d8f0b53011
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-30 00:23:57 +0530

    update by script

---
+++ product/getprlist.php
@@ -0,0 +1,43 @@
+<?php
+	include '../config.php';
+
+	date_default_timezone_set("Asia/Kolkata");
+
+	$json = file_get_contents("php://input");
+	if ($json) {
+		$obj = json_decode($json);
+		
+		$cid = $obj->cid;
+
+		$sql = "select * from product where cid = '{$cid}'";
+		$result = $conn->query($sql);
+
+		echo "[";
+		while($row = $result->fetch_assoc()){
+			$pid = $row['id'];
+			$cid = $row['cid'];
+			$name = $row['name'];
+			$price = $row['price'];
+			$sprice = $row['sprice'];
+			$description = $row['description'];
+			$qty = $row['qty'];
+	
+			echo '{
+							"pid: "'.$pid.'"
+							"cid: "'.$cid.'"
+							"name: "'.$name.'"
+							"price: "'.$price.'"
+							"sprice: "'.$sprice.'"
+							"description: "'.$description.'"
+							"qty: "'.$qty.'"
+						},';
+		}
+		echo "{}]";
+
+		// $sql = "insert into review (id, pid, rdate, name, email, comment, rating, status) values ('{$id}','{$pid}','{$rdate}','{$name}','{$email}','{$comment}','{$rating}','{$status}')";
+
+		// if($conn->query($sql)){
+		// 	echo "success";
+		// }
+	}
+?>
