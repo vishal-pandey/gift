9e40d85fff8b78158221a1a9d98de69de1e0cdfe
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-29 18:30:35 +0530

    comment

--- setup.php
+++ setup.php
@@ -10,5 +10,8 @@
 	// $conn->query($sql);
 
 	$sql = "create table orders(id varchar(15) primary key, uid varchar(15), odate datetime, theorder text, cost varchar(10), address text, status varchar(200))";
+	// $conn->query($sql);
+
+	$sql = "create table review(pid varchar(500), rdate datetime, name varchar(500), email varchar(500), comment text, rating int(5))";
 	$conn->query($sql);
 ?>
